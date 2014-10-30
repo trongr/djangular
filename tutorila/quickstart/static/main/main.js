d11050b2309de4587e17aafbaf2bb2ea25c7f67b
trong <turongr@gmail.com>
2014-10-30 00:34:04 +0000

    posts factory

--- tutorila/quickstart/static/main/main.js
+++ tutorila/quickstart/static/main/main.js
@@ -1,8 +1,8 @@
 angular.module('djangular')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ["$scope", "posts", function ($scope, posts) {
+        posts.get().success(function(data, status, headers, config){
+            $scope.posts = data.results
+        }).error(function(data, status, headers, config){
+            console.log(JSON.stringify({er:"posts get error",data:data,status:status,headers:headers,config:config}, 0, 4))
+        })
+  }]);
